802e6d83de7392fe31b5609d411cdd77765b4650
unknown <pavanyalala>
2021-06-30 18:14:45 +0530

    Employee Attendence

--- empWage.sh
+++ empWage.sh
@@ -1,1 +1,11 @@
-# Welcome to the employee Wage Computation 
+#!/bin/bash -x
+
+empCheck=$((RANDOM%2))
+
+if [ $empCheck -eq 1 ]
+then
+	echo Employee is present
+else
+	echo Employee is absent
+fi
+
